d973c05ba7e585f230cec8b180e943d8a8886c51
tanmoyopenroot <tanmoy.openroot@gmail.com>
2019-02-20 12:05:31 +0530

    add theme store

--- src/theme/themeStore.ts
+++ src/theme/themeStore.ts
@@ -2,13 +2,39 @@ import * as React from 'react';
 import { IThemeOptions } from './createTheme';
 import createTheme, { ITheme } from 'theme/createTheme';
 
-declare global {
-  interface Window {
-    __theme__: ITheme;
-  }
+interface IStore {
+  state: ITheme,
+  setThemeState: Function,
+  setters: Array<Function>,
 }
+
+let store: IStore;
 
 export const createThemeStore = (options?: IThemeOptions) => {
   const theme = createTheme(options);
-  window.__theme__ = theme;
+
+  store = {
+    state: theme,
+    setThemeState(value: ITheme) {
+      this.state = value;
+      this.setters.forEach(setter => setter(this.state));
+    },
+    setters: []
+  };
+    
+  store.setThemeState = store.setThemeState.bind(store);
 };
+
+export const useThemeStore = (): [ ITheme, Function ] => {
+  const [ state, set ] = React.useState(store.state);
+
+  if (!store.setters.includes(set)) {
+    store.setters.push(set);
+  }
+
+  React.useEffect(() => () => {
+    store.setters = store.setters.filter(setter => setter !== set)
+  }, [])
+
+  return [ state, store.setThemeState ];
+}
