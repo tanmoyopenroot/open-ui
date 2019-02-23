7bf293b622a10d93fec07505a2f27253bd5eb44b
tanmoyopenroot <tanmoy.openroot@gmail.com>
2019-02-23 20:04:37 +0530

    add common properties

--- src/common/intent.ts
+++ src/common/intent.ts
@@ -2,6 +2,7 @@ export enum Intent {
   NONE = 'none',
   PRIMARY = 'primary',
   SECONDARY = 'secondary',
+  ERROR = 'error',
 }
 
 export type TIntent = typeof Intent;
--- src/common/props.ts
+++ src/common/props.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 export type HTMLDivProps = React.HTMLAttributes<HTMLDivElement>;
+export type HTMLButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export interface IProps {
   className?: string;
