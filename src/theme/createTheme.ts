058e7b9a3abad6c4755f5b8120bbf3d50237d13f
tanmoyopenroot <tanmoy.openroot@gmail.com>
2019-03-03 20:37:37 +0530

    fix theme due change in palette

--- src/theme/createTheme.ts
+++ src/theme/createTheme.ts
@@ -11,13 +11,18 @@ import {
   spacing as defaultSpacing,
   ISpacing,
 } from './styles/spacing';
+import {
+  topography,
+  ITopography,
+} from './styles/topography';
 import elevations from './styles/elevations';
 
 export interface IThemeOptions {
-  paletteOption?: IPaletteOptions;
-  spacing?: ISpacing;
-  shape?: IShape;
-  elevations?: string[];
+  paletteOption: IPaletteOptions;
+  spacing: ISpacing;
+  shape: IShape;
+  elevations: string[];
+  topography: ITopography;
 }
 
 export interface ITheme {
@@ -25,14 +30,16 @@ export interface ITheme {
   shape: IShape;
   spacing: ISpacing;
   elevations: string[];
+  topography: ITopography;
 }
 
-export default (options: IThemeOptions = {}) : ITheme => {
+export const createTheme = (options: Partial<IThemeOptions> = {}) : ITheme => {
   const {
     paletteOption,
     spacing: spacingOption,
     shape: shapeOption,
     elevations: inputElevations,
+    topography: topographyOptions,
   } = options;
 
   const palette = createPalette(paletteOption);
@@ -43,6 +50,7 @@ export default (options: IThemeOptions = {}) : ITheme => {
     palette,
     shape,
     spacing,
+    topography: topographyOptions || topography,
     elevations: inputElevations || elevations,
   };
 
