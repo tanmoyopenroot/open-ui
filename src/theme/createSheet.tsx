edbba02f22b20afea2019697d6291f099b03bd5c
tanmoyopenroot <tanmoy.openroot@gmail.com>
2019-03-25 17:44:05 +0530

    update sheet

--- src/theme/createSheet.tsx
+++ src/theme/createSheet.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import jss, { Styles, Classes } from 'jss';
 
-export const useJSS = (intialData: () => Styles): [Classes, (updatedStyles: any) => void] => {
+interface IStyleObject {
+  initialStyles: Styles;
+  updatedStyles: Styles;
+}
+
+export const useJSS = (
+  styleObject: IStyleObject,
+  inputs: any[],
+): [Classes, (updatedStyles: any) => void] => {
   const [sheet, setSheet] = React.useState(() => {
-    return jss
-      .createStyleSheet(intialData(), { link: true })
+    const { initialStyles, updatedStyles } = styleObject;
+    const sheet = jss
+      .createStyleSheet(initialStyles, { link: true })
       .attach();
+
+    sheet.update(updatedStyles);
+
+    return sheet;
   });
 
   const removeSheet = () => {
@@ -17,15 +30,25 @@ export const useJSS = (intialData: () => Styles): [Classes, (updatedStyles: any)
 
   const updateSheet = (updatedStyles: Styles) => {
     if (sheet) {
+      console.log('update');
       sheet.update(updatedStyles);
     }
   };
 
   React.useEffect(
     () => () => {
+      console.log('remove');
       removeSheet();
     },
     [],
+  );
+
+  React.useEffect(
+    () => {
+      const { updatedStyles } = styleObject;
+      updateSheet(updatedStyles);
+    },
+    inputs,
   );
 
   return [
