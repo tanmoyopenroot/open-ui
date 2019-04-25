35e98fc56603d194880b5fdd695ffbe367654f33
tanmoyopenroot <tanmoy.openroot@gmail.com>
2019-04-25 18:06:45 +0530

    update storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,22 @@
-module.exports = (baseConfig, env, config) => {
+const path = require("path");
+const SRC_PATH = path.join(__dirname, '../src');
+const STORIES_PATH = path.join(__dirname, '../stories');
+
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('babel-loader'),
-    options: {
-      presets: [['react-app', { flow: false, typescript: true }]],
-    },
+    include: [SRC_PATH, STORIES_PATH],
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+        options: {
+          configFileName: './.storybook/tsconfig.json'
+        }
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
+    ],
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
